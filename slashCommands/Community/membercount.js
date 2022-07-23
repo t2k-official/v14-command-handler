e5f933ab12eab992c3879ff05ac29fc0a724788c
t2k-official <106411070+t2k-official@users.noreply.github.com>
2022-07-23 22:49:21 +0100

    Create membercount.js

---
+++ slashCommands/Community/membercount.js
@@ -0,0 +1,13 @@
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ApplicationCommandType, ButtonStyle } = require('discord.js');
+
+module.exports = {
+	name: 'invite',
+	description: "Get the bot's invite link",
+	cooldown: 3000,
+	type: ApplicationCommandType.ChatInput,
+	userPerms: [],
+	botPerms: [],
+	run: async (client, interaction) => {
+		const memberCount =
+        }
+}
